60e721f9969aa48d2cdb31de22654ace46341838
frostie <mattwiner04@gmail.com>
2025-02-05 10:17:13 -0800

    HTML

---
+++ main.py
@@ -0,0 +1,6 @@
+from server import app
+
+def main():
+    app.run(debug=True,host="0.0.0.0",port=5001)
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,3 @@
+from flask import *
+from datetime import datetime
+from models.calendarManager import c
